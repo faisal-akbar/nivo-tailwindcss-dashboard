e3dcb033787f74270b2c0e68733a355c0907ff05
faisalcep <faisalcep@yahoo.com>
2021-09-16 12:09:34 -0400

    infoReadme

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,6 +15,26 @@ const Header = () => {
             <div className="flex">
                 <FilterToggle />
                 <Toggle />
+                <div className="transition duration-500 ease-in-out rounded-full p-2">
+                    {/* <InformationCircleIcon
+                        onClick={() => window.open('https://dsfaisal.com', '_blank')}
+                        className="h-5 w-5 text-gray-500 dark:text-gray-400 text-2xl cursor-pointer"
+                    /> */}
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5 text-gray-500 dark:text-gray-400 text-2xl cursor-pointer"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        onClick={() => window.open('https://dsfaisal.com', '_blank')}
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                            clipRule="evenodd"
+                        />
+                        <title className="bg-gray-700">Developed by Faisal Akbar</title>
+                    </svg>
+                </div>
             </div>
         </header>
     );
